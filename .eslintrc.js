e79877427b08167bef6b0777bfe5cc3e3b7d608c
Немченко Роман Евгеньевич <rnemchenko@alfabank.ru>
2024-02-08 21:07:11 +0300

    feat: commitlint and eslint-plugin-unicorn

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
+    'plugin:unicorn/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
