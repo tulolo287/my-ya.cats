b1a70e27489f51aed2351787101288c7b007dabf
Mars <ilon.mars@yandex.ru>
2024-02-12 21:33:23 +0300

    build: update vite config

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 dotenv.config({ path: '../../.env' })
 
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    server: {
+      port: Number(process.env.CLIENT_PORT) || 3000,
+    },
+    define: {
+      __SERVER_PORT__: process.env.SERVER_PORT,
+    },
+    plugins: [react()],
+  })
+}
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: Number(process.env.CLIENT_PORT) || 3000,
-  },
-  define: {
-    __SERVER_PORT__: process.env.SERVER_PORT,
-  },
-  plugins: [react()],
-})
