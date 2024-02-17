94c042469beeeb189c804d6e13c88b88e48d5e38
Mars <ilon.mars@yandex.ru>
2024-02-17 23:21:55 +0300

    feat: add forum topic page

---
+++ packages/client/src/pages/ForumTopicPage/TopicCommentsList/index.tsx
@@ -0,0 +1,29 @@
+import { Typography } from '@components/typography'
+import { Paper } from '@components/paper'
+import { Space } from '@components/space'
+import { Comment } from '@core/types'
+
+import styles from './styles.module.css'
+
+type Props = {
+  comments: Comment[]
+}
+
+export const TopicCommentsList = ({ comments }: Props) => {
+  return comments.length > 0 ? (
+    <Space gap="24px" flexAlign="center" className={styles.wrapper}>
+      {comments.map(({ id, username, text }) => (
+        <Paper key={id} className={styles.container}>
+          <Space gap="16px">
+            {/*  className={styles.name} */}
+            <Typography fontSize="xl">{username}</Typography>
+
+            <Typography fontSize="l">{text}</Typography>
+          </Space>
+        </Paper>
+      ))}
+    </Space>
+  ) : (
+    <Paper className={styles.container}>No comments yet</Paper>
+  )
+}
