2708ec9d3033044134da31275398ba3ec051bc65
Немченко Роман Евгеньевич <rnemchenko@alfabank.ru>
2024-02-20 10:56:12 +0300

    feat: ui main page

--- packages/client/src/pages/MainPage/index.tsx
+++ packages/client/src/pages/MainPage/index.tsx
@@ -1,13 +1,13 @@
-import { SyntheticEvent } from 'react'
-import { useNavigate } from 'react-router-dom'
-
-import { Background } from '@components/background'
 import { Button } from '@components/button'
+import { Background } from '@components/background'
+import { CatImage } from '@components/catImage'
 import { Center } from '@components/center'
+import { Space } from '@components/space'
+import { Typography } from '@components/typography'
+import { useNavigate } from 'react-router-dom'
+import { SyntheticEvent } from 'react'
 import AuthController from '@controllers/auth-controller'
 import { routerPaths } from '@core/constants'
-
-// TODO: временно только кнопка для выхода (YAC-12)
 
 const MainPage = () => {
   const navigate = useNavigate()
@@ -28,7 +28,36 @@ const MainPage = () => {
   return (
     <Background>
       <Center>
-        <Button onClick={submitHandler}>Logout</Button>
+        <Space gap="60px">
+          <Space align="center">
+            <CatImage />
+            <Space>
+              <Typography align="center" tag="h1" fontSize="xxl">
+                Cat Runner
+              </Typography>
+              <Typography align="center" tag="h2" fontSize="l">
+                Endless runner game
+              </Typography>
+            </Space>
+          </Space>
+          <Space gap="25px">
+            <Button w="300px" onClick={() => navigate(routerPaths.profile)}>
+              PROFILE
+            </Button>
+            <Button w="300px" onClick={() => navigate(routerPaths.game)}>
+              PLAY
+            </Button>
+            <Button w="300px" onClick={() => navigate(routerPaths.leaderBoard)}>
+              LEADERBOARD
+            </Button>
+            <Button w="300px" onClick={() => navigate(routerPaths.forum)}>
+              FORUM
+            </Button>
+            <Button w="300px" onClick={submitHandler}>
+              LOG OUT
+            </Button>
+          </Space>
+        </Space>
       </Center>
     </Background>
   )
