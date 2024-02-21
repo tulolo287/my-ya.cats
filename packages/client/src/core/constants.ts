d2a540fcc904461f841bf8f397a8efe05adcf51c
Немченко Роман Евгеньевич <rnemchenko@alfabank.ru>
2024-02-21 15:30:54 +0300

    feat: validation on profile page

--- packages/client/src/core/constants.ts
+++ packages/client/src/core/constants.ts
@@ -1,3 +1,5 @@
+import { Validation } from './types'
+
 export const routerPaths = {
   login: '/login',
   signup: '/signup',
@@ -10,3 +12,51 @@ export const routerPaths = {
 }
 
 export const defaultError = 'Something went wrong...Please try again'
+
+export const validation: Record<
+  string,
+  { pattern: Validation; required: string | boolean }
+> = {
+  first_name: {
+    pattern: {
+      value: /^[A-ZЁА-Я][A-Za-zЁА-яё-]+$/,
+      message: 'Incorrect first name',
+    },
+    required: 'First name is required',
+  },
+  second_name: {
+    pattern: {
+      value: /^[A-ZЁА-Я][A-Za-zЁА-яё-]+$/,
+      message: 'Incorrect second name',
+    },
+    required: 'Second name is required',
+  },
+  login: {
+    pattern: {
+      value: /^(?=.*[A-Za-z])([\w-]){3,20}$/,
+      message: 'Incorrect login',
+    },
+    required: 'Login is required',
+  },
+  email: {
+    pattern: {
+      value: /.+@[^@]+[a-z]+\.[^@]{2,}$/,
+      message: 'Incorect email',
+    },
+    required: 'Email is required',
+  },
+  password: {
+    pattern: {
+      value: /^(?=.*[A-Z])(?=.*\d)[\dA-Za-z]{8,40}$/,
+      message: 'Incorrect password',
+    },
+    required: 'Password is required',
+  },
+  phone: {
+    pattern: {
+      value: /^[+-d]?\d{10,15}$/,
+      message: 'Incorrect phone',
+    },
+    required: 'Phone is required',
+  },
+}
--- packages/client/src/pages/ProfilePage/EditPasswordModalContent/index.tsx
+++ packages/client/src/pages/ProfilePage/EditPasswordModalContent/index.tsx
@@ -1,5 +1,3 @@
-import { SyntheticEvent } from 'react'
-
 import { Button } from '@components/button'
 import { Input } from '@components/input'
 import { Space } from '@components/space'
@@ -8,58 +6,76 @@ import UserController from '@controllers/user-controller'
 import { InputTypes, UserPasswordData } from '@core/types'
 
 import styles from './styles.module.css'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { validation } from '@/core/constants'
 
 const changePassword = async (data: UserPasswordData) => {
   await UserController.changePassword(data)
 }
 
-const onSubmit = async (e: SyntheticEvent) => {
-  e.preventDefault()
-
-  const form = e.target as HTMLFormElement
-  const formData = new FormData(form)
-
-  const formJson = Object.fromEntries(formData.entries())
-
+const onSubmit: SubmitHandler<ChangePassword> = async data => {
+  const { oldPassword, newPassword } = data
   try {
-    await changePassword(formJson as UserPasswordData)
+    await changePassword({ oldPassword, newPassword })
   } catch (error) {
     console.log(error)
   }
 }
 
+type ChangePassword = UserPasswordData & { secondNewPassword: string }
+
 export const EditPasswordModalContent = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ChangePassword>()
+
   return (
     <Space gap="32px">
       <Typography align="center" tag="h2" fontSize="xl" color="white">
         Change password
       </Typography>
 
-      <form className={styles.form} onSubmit={onSubmit}>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <Space gap="24px" className={styles.content}>
           <Input
             type={InputTypes.password}
             label="Old password"
-            name="oldPassword"
             w="100%"
             h="48px"
+            {...register('oldPassword', { ...validation.password })}
+            errorMessage={errors.oldPassword?.message}
           />
           <Input
             type={InputTypes.password}
             label="New password"
-            name="newPassword"
             w="100%"
             h="48px"
+            {...register('newPassword', {
+              ...validation.password,
+              validate: (value, formValues) =>
+                value !== formValues.oldPassword ||
+                'The new password must be different from the old password',
+            })}
+            errorMessage={errors.newPassword?.message}
           />
           <Input
             type={InputTypes.password}
             label="Repeat new password"
-            name="repeatPassword"
             w="100%"
             h="48px"
+            {...register('secondNewPassword', {
+              ...validation.password,
+              validate: (value, formValues) =>
+                value === formValues.newPassword || 'Must match new password',
+            })}
+            errorMessage={errors.secondNewPassword?.message}
           />
 
-          <Button color="orange">Save</Button>
+          <Button type="submit" color="orange">
+            Save
+          </Button>
         </Space>
       </form>
     </Space>
