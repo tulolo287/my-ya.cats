2f939aaae8b3c7bc312c872eb031251cb875aa20
Sayana <sayanasay@gmail.com>
2024-04-06 20:40:33 +0800

    fix: вынесены в папку миддлвары

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -4,13 +4,12 @@ dotenv.config()
 import express, { RequestHandler } from 'express'
 import cors from 'cors'
 import { json } from 'body-parser'
-import { createProxyMiddleware } from 'http-proxy-middleware'
 import cookieParser from 'cookie-parser'
 import { dbConnect } from './db'
 import router from './router/index'
-import { AuthService } from './services/auth-service'
+import { auth } from './middlewares/auth'
+import { yandexApiProxy } from './middlewares/yandex-api-proxy'
 
-const API_URL = process.env.API_URL
 const CLIENT_URL = process.env.CLIENT_URL
 
 async function startServer() {
@@ -25,49 +24,10 @@ async function startServer() {
 
   const port = Number(process.env.SERVER_PORT)
 
-  /**
-   * Проксирование запросов на апи практикума
-   */
-  app.use(
-    '/yandex-api',
-    createProxyMiddleware({
-      changeOrigin: true,
-      cookieDomainRewrite: {
-        '*': '',
-      },
-      pathRewrite: {
-        '/yandex-api': '',
-      },
-      target: API_URL,
-    })
-  )
-
+  app.use('/yandex-api', yandexApiProxy)
   app.use(json())
   app.use(cookieParser() as RequestHandler)
-
-  /**
-   * Проверяем авторизацию пользователя
-   * Если авторизован, устанавливаем в req.currentUser
-   */
-  app.use(
-    '/api',
-    async (req, res, next) => {
-      try {
-        const authService = new AuthService(req.headers.cookie)
-        const currentUser = await authService.getCurrentUser()
-        if (!currentUser) {
-          res.status(403).send('You do not have permission to access')
-          return
-        }
-        ;(req as any).currentUser = currentUser
-        next()
-      } catch (error) {
-        console.log(error)
-        res.status(500).send('Internal Server Error')
-      }
-    },
-    router
-  )
+  app.use('/api', auth, router)
 
   await dbConnect()
 
