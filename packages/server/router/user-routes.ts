40ad8498c59c0648dd51779e0409319401c3d321
Mars <ilon.mars@yandex.ru>
2024-04-08 21:54:26 +0300

    fix: remove test changes

--- packages/server/router/user-routes.ts
+++
@@ -1,8 +0,0 @@
-import userController from '../controllers/user-controller'
-import express from 'express'
-
-export const userRoutes = express.Router()
-
-userRoutes.get('/login', userController.login)
-userRoutes.get('/register', userController.register)
-userRoutes.get('/', userController.getUser)
