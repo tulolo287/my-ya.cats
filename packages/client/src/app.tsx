a28bf1aa8b614d66757eb76a193a4bfb7c590764
Nemchenko Roman <108630404+RNemch@users.noreply.github.com>
2024-04-19 11:30:10 +0300

    fix: url (#49)
    Co-authored-by: Немченко Роман Евгеньевич <rnemchenko@alfabank.ru>

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -10,8 +10,8 @@ const router = createBrowserRouter(routes)
 function App() {
   useEffect(() => {
     const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
+      const url = process.env.SERVER_URL
+      const response = await fetch(url!)
       const data = await response.json()
       console.log(data)
     }
