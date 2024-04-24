6fb6f497af1e039d4924332a6f61d9dce3b8aa4a
Sayana <sayanasay@gmail.com>
2024-04-25 00:41:55 +0800

    feat: конфиг nginx

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter(routes)
 function App() {
   useEffect(() => {
     const fetchServerData = async () => {
-      const url = process.env.SERVER_URL
+      const url = `${process.env.SERVER_URL}/api`
       const response = await fetch(url!)
       const data = await response.json()
       console.log(data)
